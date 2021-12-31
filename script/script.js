79dd6699c71444853beacfc7698efbf1e1703dc1
Jide <jideman2k@yandex.ru>
2021-12-31 22:04:31 +0400

    glacy icecream commited

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// const main_header = document.querySelector('h1');
+
+// Velocity(main_header, {opacity: 1}, 3000);
+
+// const main_header = document.querySelector('h1');
+// main_header.velocity({opacity: 0.5}, 1000);
+
+let name = 'Smith';
+
+console.log(name);
+const obj = {
+  sayHello() {
+    console.log('Hello');
+  },
+};
+console.log(obj.sayHello());
